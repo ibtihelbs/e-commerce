ce22774f29f8ec31ab720e131e0551d584e8ff31
ibtihelbs <ibtihel.bensalah@outlook.fr>
2023-03-09 16:44:08 +0100

    test

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -60,6 +60,7 @@ const Navbar = () => {
                   <circle cx="10" cy="10" r="7" />
                   <line x1="21" y1="21" x2="15" y2="15" />
                 </svg>
+                <li>li</li>
               </SearchContainer>
             </Left>
             <Center>
